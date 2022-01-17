996e6999db58e39e3d7a6ae065c10bd13fcb7e94
Nicolas Inden <nicolas.inden@innoq.com>
2022-01-17 21:09:22 +0100

    Use nunjucks. Proper faucet config.

--- faucet.config.js
+++ faucet.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   sass: [
     {
-      source: './src/scss/index.scss',
+      source: './src/ui/scss/index.scss',
       target: './public/css/app.css',
+    },
+  ],
+  js: [
+    {
+      source: './src/ui/js/index.js',
+      target: './public/js/app.js',
     },
   ],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as nunjucks from 'nunjucks';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const express = app.getHttpAdapter().getInstance();
+  const assets = join(__dirname, '..', 'public');
+  const views = join(__dirname, '..', 'src', 'ui', 'views');
 
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
+  nunjucks.configure(views, {
+    autoescape: true,
+    watch: true,
+    noCache: process.env.NODE_ENV === 'local' ? true : false,
+    express,
+  });
+
+  app.useStaticAssets(assets);
+  app.setBaseViewsDir(views);
+  app.setViewEngine('njk');
 
   await app.listen(3000);
 }
